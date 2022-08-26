eb11b5d4ec4f4d8f30405553c26c09034ab39be4
Trantarius <leborjt@yahoo.com>
2022-08-25 21:01:38 -0400

    switch to wav file method

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
  
 #include "CUtil/util.hpp"
+#include "AudioFile.h"
 
-int main(){
-    print("hi");
+int main()
+{
+    AudioFile<double> wav;
+    print("sample rate: ",wav.getSampleRate());
+    print("bit depth: ",wav.getBitDepth());
+    print("num channels: ",wav.getNumChannels());
+    print("mono? ",wav.isMono());
+    print("stereo? ",wav.isStereo());
+    print("num samples: ",wav.getNumSamplesPerChannel());
 }
