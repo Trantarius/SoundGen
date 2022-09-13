3aced55a58927e101e767cf0a72762ce9237cc8b
Trantarius <leborjt@yahoo.com>
2022-09-13 16:31:32 -0400

    add png++ library

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,16 @@
 #include <cmath>
 #include "AudioFile.h"
 #include "sound.hpp"
-
+#include "png++/png.hpp"
 
 int main()
 {
     Sound wav(1);
     SineWave(100,0,1).gen_sound(wav);
 
-    wav=wav*(wav*wav);
+    //wav=wav*(wav*wav);
 
     wav.save("out.wav");
+    system("eog pic.png &");
     system("play out.wav");
 }
--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
  
-g++ -w -std=c++17 $(find src -name "*.cpp") -I. -o exe
+COMP="g++ -w -std=c++17 $(find src -name "*.cpp") -I. -o exe"
+echo $COMP
+$COMP
 echo
 echo compiled
 echo
